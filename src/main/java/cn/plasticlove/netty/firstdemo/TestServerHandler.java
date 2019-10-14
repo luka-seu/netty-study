b83e131dd36553e9340e8ebc5ad545ce7e081dfa
dengguangming <gmdeng@plasticlove.com>
2019-10-14 19:57:45 +0800

    âfirst netty demo

---
+++ src/main/java/cn/plasticlove/netty/firstdemo/TestServerHandler.java
@@ -0,0 +1,23 @@
+package cn.plasticlove.netty.firstdemo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author Luka
+ */
+public class TestServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject httpObject) throws Exception {
+        ByteBuf byteBuf = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,byteBuf);
+        response.headers().add(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+        response.headers().add(HttpHeaderNames.CONTENT_LENGTH,byteBuf.readableBytes());
+        ctx.writeAndFlush(response);
+    }
+}
---
+++ src/main/java/cn/plasticlove/netty/firstdemo/TestServerInitializer.java
@@ -0,0 +1,19 @@
+package cn.plasticlove.netty.firstdemo;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author Luka
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("httpServerCodec",new HttpServerCodec());
+        pipeline.addLast("testServerHandler",new TestServerHandler());
+    }
+}
